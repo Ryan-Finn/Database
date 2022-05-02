b58c2a1c868cbbd685f9ea28a5049cf6cd92c4b2
Finn, Ryan <ryan.finn@mines.sdsmt.edu>
2022-05-01 18:24:24 -0600

    ...

--- Database.py
+++ Database.py
@@ -11,11 +11,13 @@ from advanced import ADVANCED
 # noinspection PyUnboundLocalVariable
 class Database:
     def __init__(self, database_name):
+        self.__database_name = database_name
+
         config = {
-            'user': 'root',
-            'password': 'P56?rN8C$fo?b?yR',
-            'host': '127.0.0.1',
-            'raise_on_warnings': True,
+            "user": "root",
+            "password": "P56?rN8C$fo?b?yR",
+            "host": "127.0.0.1",
+            "raise_on_warnings": True,
         }
 
         print("\nCONNECTING TO DATABASE...")
@@ -24,35 +26,40 @@ class Database:
             cnx = mysql.connector.connect(**config)
             cursor = cnx.cursor()
         except mysql.connector.Error as err:
-            print("  Could not connect to database `{}`.".format(database_name))
-            print("    ", err, "\n")
+            print(f"  Could not connect to database `{database_name}`.")
+            print(f"    {err}\n")
             exit(1)
 
         try:
-            cursor.execute("DROP DATABASE IF EXISTS {}".format(database_name))
-            cursor.execute("USE {}".format(database_name))
+            cursor.execute(f"USE {database_name}")
+            print("CONNECTED TO DATABASE")
         except mysql.connector.Error as err:
-            print("  Database `{}` does not exist.".format(database_name))
-            if err.errno == errorcode.ER_DB_DROP_EXISTS or err.errno == errorcode.ER_BAD_DB_ERROR:
+            print(f"  Database `{database_name}` does not exist.")
+            if (
+                err.errno == errorcode.ER_DB_DROP_EXISTS
+                or err.errno == errorcode.ER_BAD_DB_ERROR
+            ):
                 self.__createDatabase(cursor, cnx, database_name)
                 self.__createTables(cursor)
                 cnx.commit()
             else:
-                print("    ", err, "\n")
+                print(f"    {err}\n")
                 self.close()
                 exit(1)
         print()
-        self.cnx = cnx
-        self.cursor = cursor
+        self.__cnx = cnx
+        self.__cursor = cursor
 
     def __createDatabase(self, cursor, cnx, database_name):
         try:
-            cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'UTF8MB4'".format(database_name))
-            print("  Database `{}` created successfully.".format(database_name))
+            cursor.execute(
+                f"CREATE DATABASE {database_name} DEFAULT CHARACTER SET 'UTF8MB4'"
+            )
+            print(f"  Database `{database_name}` created successfully.")
             cnx.database = database_name
         except mysql.connector.Error as err:
-            print("  Database `{}` creation failed.".format(database_name))
-            print("    ", err, "\n")
+            print(f"  Database `{database_name}` creation failed.")
+            print(f"    {err}\n")
             self.close()
             exit(1)
 
@@ -61,21 +68,21 @@ class Database:
         for table_name in TABLES:
             try:
                 cursor.execute(TABLES[table_name])
-                with open('Initializations/' + table_name + '.csv') as file:
+                with open(f"Initializations/{table_name}.csv") as file:
                     for line in reader(file):
                         cursor.execute(INSERTS[table_name], tuple(line))
-                print("    Table `{}` created successfully.".format(table_name))
+                print(f"    Table `{table_name}` created successfully.")
             except mysql.connector.Error as err:
-                print("    Table `{}` creation failed.".format(table_name))
-                print("      ", err)
+                print(f"    Table `{table_name}` creation failed.")
+                print(f"      {err}")
 
     def insert(self, table_name, args):
         statement = INSERTS[table_name] % args
-        print(statement + ":")
+        print(f"{statement}:")
         try:
-            self.cursor.execute(statement)
-            print("  Inserted into row", self.cursor.lastrowid)
-            self.cnx.commit()
+            self.__cursor.execute(statement)
+            print(f"  Inserted into row {self.__cursor.lastrowid}")
+            self.__cnx.commit()
         except mysql.connector.Error as err:
             print(err)
 
@@ -83,9 +90,9 @@ class Database:
         statement = UPDATES[table_name] % args
         print(statement)
         try:
-            self.cursor.execute(statement)
-            print("Updated", self.cursor.rowcount, "rows")
-            self.cnx.commit()
+            self.__cursor.execute(statement)
+            print(f"Updated {self.__cursor.rowcount} rows")
+            self.__cnx.commit()
         except mysql.connector.Error as err:
             print(err)
 
@@ -101,31 +108,34 @@ class Database:
             args = tuple(args)
         statement = QUERIES[table_name] % args
         try:
-            self.cursor.execute(statement)
+            self.__cursor.execute(statement)
         except mysql.connector.Error as err:
             print(err)
             return []
 
         results = []
-        for row in self.cursor:
+        for row in self.__cursor:
             results.append(row)
-        print(statement + ":", *results, sep="\n  ")
+        print(f"{statement}:", *results, sep="\n  ")
         return results
 
     def advanced(self, inst, args):
         statement = ADVANCED[inst] % args
         try:
-            self.cursor.execute(statement)
+            self.__cursor.execute(statement)
         except mysql.connector.Error as err:
             print(err)
             return []
 
         results = []
-        for row in self.cursor:
+        for row in self.__cursor:
             results.append(row)
-        print(statement + ":", *results, sep="\n  ")
+        print(f"{statement}:", *results, sep="\n  ")
         return results
 
     def close(self):
-        self.cursor.close()
-        self.cnx.close()
+        # self.__cursor.execute("DROP DATABASE IF EXISTS {}".format(self.__database_name))
+        print("CLOSING CONNECTION...")
+        self.__cursor.close()
+        self.__cnx.close()
+        print("DISCONNECTED FROM DATABASE")
--- main.py
+++ main.py
@@ -4,48 +4,50 @@ from Database import Database
 
 
 def parse(args):
-    args = re.sub(r' *, *', ',', args)
-    args = re.sub(r' *= *', '=', args)
+    args = re.sub(r" *, *", ",", args)
+    args = re.sub(r" *= *", "=", args)
     args = args.replace(" ", "#").replace(",", ", ").replace("=", " = ")
     return tuple(args.split("#"))
 
 
 class main(cmd.Cmd):
+    intro = "Type `help` for more information.\nType `quit` to exit the program.\n"
+
     def __init__(self):
         super().__init__()
-        self.database = Database('test')
+        self.database = Database("test")
 
     def do_getCustomersOfProduct(self, args):
         args = parse(args)
-        self.database.advanced('foo', args)
-        print()
+        self.database.advanced("foo", args)
 
     def do_getShipmentsForCustomer(self, args):
         args = parse(args)
-        self.database.advanced('foo', args)
-        print()
+        self.database.advanced("foo", args)
 
     def do_help(self, args):
-        string = "Database tables:\n" \
-                 "=======\n" \
-                 "  staff:      staff_no, first_name, last_name, sex, birth_date, hire_date\n" \
-                 "  customers:  customer_no, first_name, last_name\n" \
-                 "  products:   product_no, unit_price, quantity, locations\n" \
-                 "  shipments:  shipment_no, status, shipment_date, delivery_date\n" \
-                 "  orders:     order_no, shipment_no, customer_no, order_date, products\n" \
-                 "\nAttribute formats:\n" \
-                 "===========\n" \
-                 "  staff_no, product_no, shipment_no, order_no: set automatically\n" \
-                 "  birth_date, hire_date, ship_date, delivery_date, order_date: YYYY-MM-DD\n" \
-                 "  sex:                    M, F, or OTHER\n" \
-                 "  status:                 PROCESSING, IN-TRANSIT, or DELIVERED\n" \
-                 "  locations, products:    \"a, b, c, etc.\"\n" \
-                 "\nExamples:\n" \
-                 "=========\n" \
-                 "  Inserting:  insert staff Ryan Finn M 1998-10-01 2022-05-06\n" \
-                 "  Updating:   update staff first_name=Bryan staff_no=1\n" \
-                 "  Deleting:   delete staff 3\n" \
-                 "  Querying:   query staff first_name, last_name, birth_date sex = F"
+        string = (
+            "Database tables:\n"
+            "================\n"
+            "  staff:      staff_no, first_name, last_name, sex, birth_date, hire_date\n"
+            "  customers:  customer_no, first_name, last_name\n"
+            "  products:   product_no, unit_price, quantity, locations\n"
+            "  shipments:  shipment_no, status, shipment_date, delivery_date\n"
+            "  orders:     order_no, shipment_no, customer_no, order_date, products\n"
+            "\nAttribute formats:\n"
+            "==================\n"
+            "  staff_no, product_no, shipment_no, order_no: set automatically\n"
+            "  birth_date, hire_date, ship_date, delivery_date, order_date: YYYY-MM-DD\n"
+            "  sex:                    M, F, or OTHER\n"
+            "  status:                 PROCESSING, IN-TRANSIT, or DELIVERED\n"
+            '  locations, products:    "a, b, c, etc."\n'
+            "\nExamples:\n"
+            "=========\n"
+            "  Inserting:  insert staff Ryan Finn M 1998-10-01 2022-05-06\n"
+            "  Updating:   update staff first_name=Bryan staff_no=1\n"
+            "  Deleting:   delete staff 3\n"
+            "  Querying:   query staff first_name, last_name, birth_date sex = F"
+        )
         print(string)
         super().do_help(args)
 
@@ -53,37 +55,37 @@ class main(cmd.Cmd):
         """insert [table] [data]
         Example: insert staff 'Ryan' 'Finn' M 1998-10-01 2022-05-06"""
         args = parse(args)
-        self.database.insert(args[0], args[1:len(args)])
-        print()
+        self.database.insert(args[0], args[1 : len(args)])
 
     def do_update(self, args):
         """update [table] [set] [where]
         Example: update staff first_name=Bryan staff_no=1"""
         args = parse(args)
-        self.database.update(args[0], args[1:len(args)])
-        print()
+        self.database.update(args[0], args[1 : len(args)])
 
     def do_delete(self, args):
         """delete [table] [data]
         Delete [data] from [table]"""
         args = parse(args)
-        self.database.delete(args[0], args[1:len(args)])
-        print()
+        self.database.delete(args[0], args[1 : len(args)])
 
     def do_query(self, args):
         """query [table] [select] [where]
         Query [select] from [table] where [where]"""
         args = parse(args)
-        self.database.query(args[0], args[1:len(args)])
-        print()
+        self.database.query(args[0], args[1 : len(args)])
 
     def do_quit(self, _):
         """Close database connection and exit program"""
         self.database.close()
         return True
 
+    def postcmd(self, stop, line):
+        print()
+        return super().postcmd(stop, line)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main().cmdloop()
 
 # data_employee = {
